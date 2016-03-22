5bc181181e3ff0e3f660e8e9aee2bc7930e29980
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-22 10:24:16 -0700

    Fixed scope issue, all functions working so far.

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
-var salesData = [eightAM, nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM, sixPM, sevenPM, eightPM, ninePM, tenPM, elevenPM, twelveAM, oneAM, twoAM];
 var pizzasSoldPerHour = [];
 var deliveriesMadePerHour = [];
-
-//Generates a random number of pizzas delivered in a given hour given a range of min to max.
-function pizzasPerHour(minPizzas, maxPizzas) {
-  return Math.floor(Math.random() * (maxPizzas - minPizzas) + minPizzas);
-};
-console.log(pizzasPerHour(3,4));
-
-//Generates a random number of deliveries made in a given hour given a range of min to max.
-function deliveriesPerHour(minDeliveries, maxDeliveries) {
-  return Math.floor(Math.random() * (maxPizzas - minPizzas) + minPizzas);
-};
-
-//References the salesData array, takes a time object and reads the value of the pizzasSold property, then pushes that value to the array PizzasSoldPerHour.
-function salesDataPerDay() {
-  for (i=0; i < salesData.length; i++) {
-    pizzasSoldPerHour.push(salesData[i].pizzasSold);
-  }
-  return pizzasSoldPerHour;
-}
-
-//eferences the salesData array, takes a time object and reads the value of the deliveriesMade property, then pushes that value to the array deliveriesMadePerHour.
-function deliveriesDataPerDay() {
-  for (i=0; i < salesData.length; i++) {
-    deliveriesMadePerHour.push(salesData[i].deliveriesMade);
-  }
-  return pizzasSoldPerHour;
-}
+var minPizzas;
+var maxPizzas;
+var minDeliveries;
+var maxDeliveries;
+var pizzasSold;
 
 var eightAM = {
   time: '8:00am',
@@ -49,14 +26,123 @@ var tenAM = {
 
 var elevenAM = {
   time: '11:00am',
-  pizzasSold: pizzasPerHour(0,4),
+  pizzasSold: pizzasPerHour(0,7),
   deliveriesMade: deliveriesPerHour(0,4)
 };
 
-var specificLocation = {
-  this.locationName = locationName;
-  this.pizzasSoldPerHour = salesDataPerDay();
-  this.deliveries
+var twelvePM = {
+  time: '12:00pm',
+  pizzasSold: pizzasPerHour(0,7),
+  deliveriesMade: deliveriesPerHour(0,4)
 };
 
-var ballardLocation = new specificLocation('Ballard', salesData)
+var onePM = {
+  time: '1:00pm',
+  pizzasSold: pizzasPerHour(0,7),
+  deliveriesMade: deliveriesPerHour(0,4)
+};
+
+var twoPM = {
+  time: '2:00pm',
+  pizzasSold: pizzasPerHour(2,15),
+  deliveriesMade: deliveriesPerHour(1,4)
+};
+
+var threePM = {
+  time: '3:00pm',
+  pizzasSold: pizzasPerHour(2,15),
+  deliveriesMade: deliveriesPerHour(1,4)
+};
+
+var fourPM = {
+  time: '4:00pm',
+  pizzasSold: pizzasPerHour(2,15),
+  deliveriesMade: deliveriesPerHour(1,4)
+};
+
+var fivePM = {
+  time: '5:00pm',
+  pizzasSold: pizzasPerHour(15,35),
+  deliveriesMade: deliveriesPerHour(3,8)
+};
+
+var sixPM = {
+  time: '6:00pm',
+  pizzasSold: pizzasPerHour(15,35),
+  deliveriesMade: deliveriesPerHour(3,8)
+};
+
+var sevenPM = {
+  time: '7:00pm',
+  pizzasSold: pizzasPerHour(15,35),
+  deliveriesMade: deliveriesPerHour(3,8)
+};
+
+var eightPM = {
+  time: '8:00pm',
+  pizzasSold: pizzasPerHour(12,31),
+  deliveriesMade: deliveriesPerHour(5,12)
+};
+
+var ninePM = {
+  time: '9:00pm',
+  pizzasSold: pizzasPerHour(12,31),
+  deliveriesMade: deliveriesPerHour(5,12)
+};
+
+var tenPM = {
+  time: '10:00pm',
+  pizzasSold: pizzasPerHour(12,31),
+  deliveriesMade: deliveriesPerHour(5,12)
+};
+
+var elevenPM = {
+  time: '11:00pm',
+  pizzasSold: pizzasPerHour(5,20),
+  deliveriesMade: deliveriesPerHour(6,11)
+};
+
+var twelveAM = {
+  time: '12:00am',
+  pizzasSold: pizzasPerHour(5,20),
+  deliveriesMade: deliveriesPerHour(6,11)
+};
+
+var oneAM = {
+  time: '1:00am',
+  pizzasSold: pizzasPerHour(5,20),
+  deliveriesMade: deliveriesPerHour(6,11)
+};
+
+var salesData = [eightAM, nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM, sixPM, sevenPM, eightPM, ninePM, tenPM, elevenPM, twelveAM, oneAM];
+
+//Generates a random number of pizzas delivered in a given hour given a range of min to max.
+function pizzasPerHour(minPizzas, maxPizzas) {
+  return Math.floor(Math.random() * (maxPizzas - minPizzas) + minPizzas);
+};
+
+//Generates a random number of deliveries made in a given hour given a range of min to max.
+function deliveriesPerHour(minDeliveries, maxDeliveries) {
+  return Math.floor(Math.random() * (maxDeliveries - minDeliveries) + minDeliveries);
+};
+
+//References the salesData array, takes a time object and reads the value of the pizzasSold property, then pushes that value to the array PizzasSoldPerHour.
+function salesDataPerDay() {
+  for (var i = 0; i < salesData.length; i++) {
+    pizzasSoldPerHour.push(salesData[i].pizzasSold);
+  }
+  return pizzasSoldPerHour;
+}
+console.log(salesDataPerDay());
+
+//References the salesData array, takes a time object and reads the value of the deliveriesMade property, then pushes that value to the array deliveriesMadePerHour.
+function deliveriesDataPerDay() {
+  for (i = 0; i < salesData.length; i++) {
+    deliveriesMadePerHour.push(salesData[i].deliveriesMade);
+  }
+  return deliveriesMadePerHour;
+}
+console.log(deliveriesDataPerDay());
+
+//
+function writeDataForLocation() {
