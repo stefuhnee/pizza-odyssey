be3bc4e7b5427abe929377ea127eb6c9d8d6550b
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-22 09:28:05 -0700

    Made function to cycle through sales data array, retrieve specific object and its pizzaSold properties, then push that value to an array

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var salesData = [eightAM, nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM, sixPM, sevenPM, eightPM, ninePM, tenPM, elevenPM, twelveAM, oneAM, twoAM];
+var pizzasSoldPerHour = [];
 
 function pizzasPerHour(minPizzas, maxPizzas) {
   return Math.floor(Math.random() * (maxPizzas - minPizzas) + minPizzas);
 };
+console.log(pizzasPerHour(3,4));
 
 function deliveriesPerHour(minDeliveries, maxDeliveries) {
   return Math.floor(Math.random() * (maxPizzas - minPizzas) + minPizzas);
 };
+
+function salesDataPerDay() {
+  for (i=0; i < salesData.length; i++) {
+    pizzasSoldPerHour.push(salesData[i].pizzasSold);
+  }
+  return pizzasSoldPerHour;
+}
 
 var eightAM = {
   time: '8:00am',
@@ -34,5 +43,7 @@ var elevenAM = {
 
 var specificLocation = {
   this.locationName = locationName;
-  this.salesData = salesData;
+  this.pizzasSoldPerHour =
 };
+
+var ballardLocation = new specificLocation('Ballard', salesData)
