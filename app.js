8a6f04843e0c81eb1a902b22ffa7f7968b0c1476
Stefanie Hansen <stefuhnee@gmail.com>
2016-03-23 11:38:28 -0700

    Got tables to print correctly for ballard. need to work on filling it with info

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ storeLocation.prototype.pushHourlyData = function(data) {
   this.hourlyData.push(data);
 };
 
-// Assign properties to hourlyData object and calculate random number of pizzas sold, random number of deliveries made, and the drivers needed.
+// Creates new object HourlyData with 4 properties. Eventually becomes a property of the storeLocation.
 function HourlyData(time, minPizzasSold, maxPizzasSold, minDeliveriesMade, maxDeliveriesMade) {
   this.time = time;
   this.pizzasSold = getRandomNumber(minPizzasSold, maxPizzasSold);
@@ -30,227 +30,63 @@ var southLakeUnion = new storeLocation('south-lake-union');
 var georgetown = new storeLocation('georgetown');
 var ravenna = new storeLocation('ravenna');
 
-// Add values to the hourlyData properties. Needs to be done for every hour.
+// Creating new HourlyData objects with associated properties. Needs to be done for every hour.
 ballard.pushHourlyData(new HourlyData('8:00 am', 0, 3, 1, 7));
-console.log(ballard.hourlyData);
+ballard.pushHourlyData(new HourlyData('9:00 am', 0, 3, 1, 7));
+ballard.pushHourlyData(new HourlyData('10:00 am', 0, 3, 1, 7));
+ballard.pushHourlyData(new HourlyData('11:00 am', 5, 10, 2, 8));
+ballard.pushHourlyData(new HourlyData('12:00 pm', 5, 10, 2, 8));
+ballard.pushHourlyData(new HourlyData('1:00 pm', 5, 10, 2, 8));
+ballard.pushHourlyData(new HourlyData('2:00 pm', 2, 13, 1, 7));
+ballard.pushHourlyData(new HourlyData('3:00 pm', 2, 13, 1, 7));
+ballard.pushHourlyData(new HourlyData('4:00 pm', 2, 13, 1, 7));
+ballard.pushHourlyData(new HourlyData('5:00 pm', 0, 15, 2, 9));
+ballard.pushHourlyData(new HourlyData('6:00 pm', 0, 15, 2, 9));
+ballard.pushHourlyData(new HourlyData('7:00 pm', 0, 15, 2, 9));
+ballard.pushHourlyData(new HourlyData('8:00 pm', 1, 3, 4, 12));
+ballard.pushHourlyData(new HourlyData('9:00 pm', 1, 3, 4, 12));
+ballard.pushHourlyData(new HourlyData('10:00 pm', 1, 3, 4, 12));
+ballard.pushHourlyData(new HourlyData('11:00 pm', 8, 15, 6, 16));
+ballard.pushHourlyData(new HourlyData('12:00 am', 8, 15, 6, 16));
+ballard.pushHourlyData(new HourlyData('1:00 am', 8, 15, 6, 16));
 
-// function generateHeadingRow(tableHeadings) {
-//   var row = document.createElement('tr');
-//   var col;
-//   for (var i = 0; i < tableHeadings.length; i++) {
-//     col = document.createElement('th');
-//     col.textContent = tableHeadings[i];
-//     row.appendChild(col);
-//   }
-//   return row;
-// }
-//
-// function generateRow(rowArray) {
-//   var row = document.createElement('tr');
-//   var col;
-//   for (var i = 0; i < rowArray.length; i++) {
-//     col = document.createElement('td');
-//     col.textContent = rowArray[i];
-//     row.appendChild(col);
-//   }
-//   return row;
-// }
-//
-// function getEightAM() {
-//   var eightAM = {
-//     time: '8:00am',
-//     pizzasSold: this.pizzasPerHour(),
-//     deliveriesMade: this.deliveriesPerHour()
-//   };
-//   return eightAM;
-// };
-//
-// function getNineAM() {
-//   var nineAM = {
-//     time: '9:00am',
-//     pizzasSold: pizzasPerHour(0,4),
-//     deliveriesMade: deliveriesPerHour(0,4)
-//   };
-//   return nineAM;
-// };
-//
-// function getTenAM() {
-//   var tenAM = {
-//     time: '10:00am',
-//     pizzasSold: pizzasPerHour(0,4),
-//     deliveriesMade: deliveriesPerHour(0,4)
-//   };
-//   return tenAM;
-// };
-//
-// function getElevenAM() {
-//   var elevenAM = {
-//     time: '11:00am',
-//     pizzasSold: pizzasPerHour(0,7),
-//     deliveriesMade: deliveriesPerHour(0,4)
-//   };
-//   return elevenAM;
-// };
-//
-// function getTwelvePM() {
-//   var twelvePM = {
-//     time: '12:00pm',
-//     pizzasSold: pizzasPerHour(0,7),
-//     deliveriesMade: deliveriesPerHour(0,4)
-//   };
-//   return twelvePM;
-// };
-//
-// function getOnePM() {
-//   var onePM = {
-//     time: '1:00pm',
-//     pizzasSold: pizzasPerHour(0,7),
-//     deliveriesMade: deliveriesPerHour(0,4)
-//   };
-//   return onePM;
-// };
-//
-// function getTwoPM() {
-//   var twoPM = {
-//     time: '2:00pm',
-//     pizzasSold: pizzasPerHour(2,15),
-//     deliveriesMade: deliveriesPerHour(1,4)
-//   };
-//   return twoPM;
-// };
-//
-// function getThreePM() {
-//   var threePM = {
-//     time: '3:00pm',
-//     pizzasSold: pizzasPerHour(2,15),
-//     deliveriesMade: deliveriesPerHour(1,4)
-//   };
-//   return threePM;
-// };
-//
-// function getFourPM() {
-//   var fourPM = {
-//     time: '4:00pm',
-//     pizzasSold: pizzasPerHour(2,15),
-//     deliveriesMade: deliveriesPerHour(1,4)
-//   };
-//   return fourPM;
-// };
-//
-// function getFivePM() {
-//   var fivePM = {
-//     time: '5:00pm',
-//     pizzasSold: pizzasPerHour(15,35),
-//     deliveriesMade: deliveriesPerHour(3,8)
-//   };
-//   return fivePM;
-// };
-//
-// function getSixPM() {
-//   var sixPM = {
-//     time: '6:00pm',
-//     pizzasSold: pizzasPerHour(15,35),
-//     deliveriesMade: deliveriesPerHour(3,8)
-//   };
-//   return sixPM;
-// };
-//
-// function getSevenPM() {
-//   var sevenPM = {
-//     time: '7:00pm',
-//     pizzasSold: pizzasPerHour(15,35),
-//     deliveriesMade: deliveriesPerHour(3,8)
-//   };
-//   return sevenPM;
-// };
-//
-// function getEightPM() {
-//   var eightPM = {
-//     time: '8:00pm',
-//     pizzasSold: pizzasPerHour(12,31),
-//     deliveriesMade: deliveriesPerHour(5,12)
-//   };
-//   return eightPM;
-// };
-//
-// function getNinePM() {
-//   var ninePM = {
-//     time: '9:00pm',
-//     pizzasSold: pizzasPerHour(12,31),
-//     deliveriesMade: deliveriesPerHour(5,12)
-//   };
-//   return ninePM;
-// };
-//
-// function getTenPM() {
-//   var tenPM = {
-//     time: '10:00pm',
-//     pizzasSold: pizzasPerHour(12,31),
-//     deliveriesMade: deliveriesPerHour(5,12)
-//   };
-//   return tenPM;
-// };
-//
-// function getElevenPM() {
-//   var elevenPM = {
-//     time: '11:00pm',
-//     pizzasSold: pizzasPerHour(5,20),
-//     deliveriesMade: deliveriesPerHour(6,11)
-//   };
-//   return elevenPM;
-// };
-//
-// function getTwelveAM() {
-//   var twelveAM = {
-//     time: '12:00am',
-//     pizzasSold: pizzasPerHour(5,20),
-//     deliveriesMade: deliveriesPerHour(6,11)
-//   };
-//   return twelveAM;
-// };
-//
-// function getOneAM() {
-//   var oneAM = {
-//     time: '1:00am',
-//     pizzasSold: pizzasPerHour(5,20),
-//     deliveriesMade: deliveriesPerHour(6,11)
-//   };
-//   return oneAM;
-// };
-//
-// //Generates a random number of deliveries made in a given hour given a range of min to max.
-// function deliveriesPerHour(minDeliveries, maxDeliveries) {
-//   return Math.floor(Math.random() * (maxDeliveries - minDeliveries) + minDeliveries);
-// };
-//
-// //Creates SpecificLocation object
-// function SpecificLocation(name, salesData, minPizzas, maxPizzas) {
-//   this.name = name;
-//   this.salesData = salesData;
-//   this.minPizzas = minPizzas;
-//   this.maxPizzas = maxPizzas
-// };
-//
-// // Adding methods to object constructor
-//
-// //Calculates the drivers needed for every hour of the day.
-// SpecificLocation.prototype.specificDrivers = function() {
-//   for (var i = 0; i < this.salesData.length; i++) {
-//     this.salesData[i].driversNeeded = [];
-//     //
-//     this.salesData[i].driversNeeded = Math.ceil(this.salesData[i].deliveriesMade / 3);
-//     console.log('sales Data of ' + i + ' ', this.salesData[i]);
-//   }
-// };
-//
-// //Generates a random number of pizzas delivered in a given hour given a range of min to max.
-// SpecificLocation.prototype.pizzasPerHour = function() {
-//   for (var i = 0; i < this.minPizzas.length; i++) {
-//     return Math.floor(Math.random() * (this.maxPizzas[i] - this.minPizzas[i]) + this.minPizzas[i]);
-//   }
-//   console.log('ballard.pizzasPerHour()');
-// };
-//
+// Creates a heading row for the table
+function generateHeadingRow(tableHeadings) {
+  var row = document.createElement('tr');
+  var col;
+  for (var i = 0; i < tableHeadings.length; i++) {
+    col = document.createElement('th');
+    col.textContent = tableHeadings[i];
+    row.appendChild(col);
+  }
+  return row;
+}
+
+// Creates a data row for the table
+function generateRow(rowArray) {
+  var row = document.createElement('tr');
+  var col;
+  for (var i = 0; i < rowArray.length; i++) {
+    col = document.createElement('td');
+    col.textContent = rowArray[i];
+    row.appendChild(col);
+  }
+  return row;
+}
+
+// Creates the table and calls functions to generate the heading and each data row, sequentially.
+var locationTable = document.createElement('table');
+var firstRow = generateHeadingRow(['Time', 'Pizzas Sold', 'Deliveries Made', 'Drivers Needed']);
+var secondRow = generateRow(['placeholder']);
+
+// Adds rows to the table.
+locationTable.appendChild(firstRow);
+locationTable.appendChild(secondRow);
+
+// Adds table to the page using the HTML id element.
+document.getElementById('ballard').appendChild(locationTable);
+console.log(locationTable);
+
 // // //Displays the required information in the format given.
 // // SpecificLocation.prototype.writeToDocument = function() {
 // //   // Cycle through each hour object in the array
